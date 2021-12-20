ef66cac29d81a320eeffaf7067025f57efba3048
DESKTOP-6R2O3C2\CXC <robo.12@outlook.es>
2021-12-20 17:53:26 -0600

    Third program example

---
+++ third_attemp.py
@@ -0,0 +1,12 @@
+class Vehicle:
+    def __init__(self,name,max_speed,miliage):
+        self.name = name
+        self.max_speed = max_speed
+        self.miliage = miliage
+        
+civic = Vehicle("rojo",200,12)
+print(civic)
+
+    
+    
+    
