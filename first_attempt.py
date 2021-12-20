a0d07fbf539cd5d06ed2451b6da351298a3d7d9e
DESKTOP-6R2O3C2\CXC <robo.12@outlook.es>
2021-12-20 16:24:27 -0600

    first commit

---
+++ first_attempt.py
@@ -0,0 +1,10 @@
+#automate the stuff borings with Python
+import sys
+
+while True:
+    print('Type to exit this program')
+    response = input()
+    if response == 'exit':
+        sys.exit()
+    print('you typed ' + response + '.')
+ 
